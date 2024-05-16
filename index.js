5b895433272f45fe063cec275c757a030887f6de
hamado1018stream <hamadodev2006@gmail.com>
2024-05-16 15:48:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tmi = require('tmi.js'),
     { channel, username, password } = require('./config.json');
 
+const port = process.env.PORT || 0000;
+
 const options = {
     options: { debug: true },
     connection: {
